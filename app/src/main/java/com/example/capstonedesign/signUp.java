1e2ebccc0615674a3a6b1ce64b1306b91279e9bb
dahye <dahye0729@sch.ac.kr>
2020-08-28 11:58:55 +0900

    sign up layout

--- app/src/main/java/com/example/capstonedesign/signUp.java
+++ app/src/main/java/com/example/capstonedesign/signUp.java
@@ -4,12 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class signUp extends AppCompatActivity {
-    Button signupbtn;
-    Button loginbtn;
-    Button rentalbtn;
-    Button mybtn;
+    Button signupbtn; //메뉴 바에 있는 회원가입 버튼
+    Button loginbtn; //메뉴 바에 있는 로그인 버튼
+    Button rentalbtn; //메뉴 바에 있는 대여 버튼
+    Button mybtn; //메뉴 바에 있는 내페이지 버튼
+    EditText userName; // EditText로 입력받는 이름
+    EditText userID; // EditText로 입력받는 아이디
+    EditText userPW; // EditText로 입력받는 비밀번호
+    EditText userBirth; // EditText로 입력받는 생년월일
+    EditText userPhone; // EditText로 입력받는 핸드폰번호
+    Button signUpCheckbtn; // 회원가입 확인 버튼
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +27,12 @@ public class signUp extends AppCompatActivity {
         rentalbtn = (Button)findViewById(R.id.rentalButton); //메뉴 바에 있는 대여 버튼
         mybtn = (Button)findViewById(R.id.myButton); //메뉴 바에 있는 내페이지 버튼
 
+        userName = (EditText)findViewById(R.id.editUserName); // EditText로 입력받는 이름
+        userID = (EditText)findViewById(R.id.editUserID); // EditText로 입력받는 아이디
+        userPW = (EditText)findViewById(R.id.editUserPW); // EditText로 입력받는 비밀번호
+        userBirth = (EditText)findViewById(R.id.editUserBirth); // EditText로 입력받는 생년월일
+        userPhone = (EditText)findViewById(R.id.editUserPhone); // EditText로 입력받는 핸드폰번호
 
+        signUpCheckbtn = (Button)findViewById(R.id.signUpCheckButton); // 회원가입 확인 버튼
     }
 }
