0f31ccf2f7b3f9538e004b7b6686be1a24324465
dahye <dahye0729@sch.ac.kr>
2020-08-28 11:51:05 +0900

    sign up layout

--- app/src/main/java/com/example/capstonedesign/signUp.java
+++ app/src/main/java/com/example/capstonedesign/signUp.java
@@ -15,10 +15,10 @@ public class signUp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-        signupbtn = (Button)findViewById(R.id.signUpButton);
-        loginbtn = (Button)findViewById(R.id.loginButton);
-        rentalbtn = (Button)findViewById(R.id.rentalButton);
-        mybtn = (Button)findViewById(R.id.myButton);
+        signupbtn = (Button)findViewById(R.id.signUpButton); //메뉴 바에 있는 회원가입 버튼
+        loginbtn = (Button)findViewById(R.id.loginButton); //메뉴 바에 있는 로그인 버튼
+        rentalbtn = (Button)findViewById(R.id.rentalButton); //메뉴 바에 있는 대여 버튼
+        mybtn = (Button)findViewById(R.id.myButton); //메뉴 바에 있는 내페이지 버튼
 
 
     }
